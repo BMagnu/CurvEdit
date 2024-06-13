2f8b2cee35a039fbdcce191b06d3925f71fa6b7e
BMagnu <6238428+BMagnu@users.noreply.github.com>
2024-06-13 21:41:21 +0200

    Properly add Builtin Curves

---
+++ fso_curves/src/curve.rs
@@ -0,0 +1,141 @@
+use std::string::ToString;
+use once_cell::sync::Lazy;
+use strum::IntoEnumIterator;
+use strum_macros::{Display, EnumIter};
+
+pub struct Curve<'a> {
+	pub name: String,
+	pub keyframes: Vec<CurveKeyframe<'a>>
+}
+
+impl Curve<'_> {
+	pub fn calculate(&self, x: f32) -> f32 {
+		assert!(self.keyframes.len() >= 2);
+
+		if self.keyframes[0].x > x {
+			return self.keyframes[0].y;
+		}
+		else if self.keyframes[self.keyframes.len() - 1].x <= x {
+			return self.keyframes[self.keyframes.len() - 1].y;
+		}
+
+		let result = self.keyframes[1..].iter().enumerate().find(|(_, kf)| x < kf.x).map(|(prev_index, kf)| {
+			let prev_kf = &self.keyframes[prev_index];
+			prev_kf.segment.calculate(x, prev_kf, kf)
+		});
+
+		if let Some(result) = result {
+			result
+		}
+		else {
+			//At this point, no keyframe was matched. Should be impossible
+			unreachable!("Keyframe not found");
+		}
+	}
+}
+
+pub struct CurveKeyframe<'a> {
+	pub x: f32,
+	pub y: f32,
+	pub segment: CurveSegment<'a>
+}
+
+pub enum CurveSegment<'a>{
+	Constant,
+	Linear,
+	Polynomial { ease_in: bool, degree: f32 },
+	Circular { ease_in: bool },
+	Subcurve { curve: &'a Curve<'a> }
+}
+impl CurveSegment<'_> {
+	pub fn calculate(&self, x: f32, current: &CurveKeyframe, next: &CurveKeyframe) -> f32 {
+		self.calculate_from_delta((x - current.x) / (next.x - current.x)) * (next.y - current.y) + current.y
+	}
+	
+	fn calculate_from_delta(&self, t: f32) -> f32 {
+		match *self{
+			CurveSegment::Constant => { 0f32 }
+			CurveSegment::Linear => { t }
+			CurveSegment::Polynomial { ease_in, degree } => {
+				if ease_in {
+					t.powf(degree)
+				}
+				else {
+					1f32 - (1f32 - t).powf(degree)
+				}
+			}
+			CurveSegment::Circular { ease_in } => {
+				if ease_in {
+					1f32 - (1f32 - t.powi(2)).sqrt()
+				}
+				else {
+					(1f32 - (1f32 - t).powi(2)).sqrt()
+				}
+			}
+			CurveSegment::Subcurve { curve } => { curve.calculate(t) }
+		}
+	}
+}
+
+pub static BUILTIN_CURVES: Lazy<Vec<Curve>> = Lazy::new(|| {
+	let mut builtins = Vec::new();
+
+	#[derive(PartialEq, EnumIter, Display)]
+	enum EASE {EaseIn, EaseOut, EaseInOut}
+
+	#[derive(Copy, Clone, EnumIter, Display)]
+	enum TYPE {Circ, Quad = 2, Cubic = 3, Quart = 4, Quint = 5}
+
+	for ease in EASE::iter() {
+		for reverse in [true, false] {
+			for interptype in TYPE::iter() {
+				let mut name = ease.to_string() + &interptype.to_string();
+				let mut keyframes: Vec<CurveKeyframe> = Vec::new();
+
+				if reverse {
+					name += "Rev";
+				}
+
+				let ease_in = ease != EASE::EaseOut;
+
+				keyframes.push(CurveKeyframe {
+					x: 0f32,
+					y: if reverse { 1f32 } else { 0f32 },
+					segment: match interptype {
+						TYPE::Circ => {
+							CurveSegment::Circular { ease_in }
+						}
+						interptype => {
+							CurveSegment::Polynomial { ease_in, degree: (interptype as i32) as f32 }
+						}
+					}
+				});
+
+				if ease == EASE::EaseInOut {
+					keyframes.push(CurveKeyframe {
+						x: 0.5f32,
+						y: 0.5f32,
+						segment: match interptype {
+							TYPE::Circ => {
+								CurveSegment::Circular { ease_in: !ease_in }
+							}
+							interptype => {
+								CurveSegment::Polynomial { ease_in: !ease_in, degree: (interptype as i32) as f32 }
+							}
+						}
+					});
+				}
+
+				keyframes.push(CurveKeyframe {
+					x: 1f32,
+					y: if reverse { 0f32 } else { 1f32 },
+					segment: CurveSegment::Constant {}
+				});
+
+				builtins.push(Curve { name, keyframes })
+			}
+		}
+	}
+
+	builtins
+});
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use fso_curves::BUILTIN_CURVES;
+
 fn main() {
-	println!("Hello, world!");
+	for builtin in BUILTIN_CURVES.iter() {
+		println!("{}", builtin.name)
+	}
+
+	let curve = BUILTIN_CURVES.iter().find(|curve| curve.name == "EaseInOutQuad");
+	if let Some(curve) = curve {
+		println!("0: {}, 0.25: {}, 0.5: {}, 0.75: {}, 1: {}",
+				 curve.calculate(0f32),
+				 curve.calculate(0.25f32),
+				 curve.calculate(0.5f32),
+				 curve.calculate(0.75f32),
+				 curve.calculate(1f32));
+	}
 }
